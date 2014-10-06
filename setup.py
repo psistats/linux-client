fa8e0ab03dbd0dcd40e5f83e9ddcbe3a861d0960
v0idnull <v0idnull@gmail.com>
2014-10-06 00:36:34 -0400

    fix version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="psistats-client",
-    version="0.0.7",
+    version="0.0.8-dev",
     description="Psistats python client",
     url="http://psistats.psikon.org",
     author="Alex D",
