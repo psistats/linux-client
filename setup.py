2874b04bd3c133baa6788e37b05d4e724e81ffe3
alex-dow <adow@psikon.com>
2014-10-05 23:58:51 -0400

    Changing version to 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="psistats-client",
-    version="0.0.4-dev",
+    version="0.0.4",
     description="Psistats python client",
     url="http://psistats.psikon.org",
     author="Alex D",
