de4944e42e7c21b3b02abf580fe1f9acc7ff299a
v0idnull <v0idnull@gmail.com>
2014-10-06 23:32:03 -0400

    some minor refactoring

--- psistats/app/main.py
+++ psistats/app/main.py
@@ -37,6 +37,80 @@ class Main(object):
             self.logger = logger
         return self.logger
 
+    def _loop(self):
+        config = self.config
+        logger = self.get_logger()
+        # used as a counter to determine interval to
+        # send uptime and ip address updates
+        meta_refresh_counter = config['app']['meta_timer']
+        sleep = config['app']['timer']
+
+        while True:
+
+            try:
+
+                if self.connected == False:
+                    logger.info("Creating connection")
+                    logger.debug("get connection")
+                    self.connection = get_connection(self.config['server'])
+
+                    logger.debug("get channel")
+                    self.channel = self.connection.channel()
+
+                    logger.debug("setup queue")
+                    setup_queue(self.channel, self.config['exchange'], self.config['queue'])
+
+                    self.connected = True
+
+                packet = {
+                    'hostname': stats.hostname(),
+                    'cpu': stats.cpu(),
+                    'mem': stats.mem()
+                }
+
+                if meta_refresh_counter == config['app']['meta_timer']:
+                    packet['ipaddr'] = stats.ip4_addresses()
+                    packet['uptime'] = stats.uptime()
+                    meta_refresh_counter = 1
+
+                if config['cpu_temp']['enabled']:
+                    packet['cpu_temp'] = stats.cpu_temp()
+
+                packet_json = json.dumps(packet)
+                logger.debug('Sending packet: %s' % packet_json)
+                send_json(packet_json, self.channel, config['exchange']['name'], config['queue']['name'])
+
+                meta_refresh_counter += 1
+
+                time.sleep(sleep)
+
+            except (AMQPConnectionError, ChannelClosed, socket.error) as e:
+                logger.critical("Connection error with RabbitMQ!")
+                logger.exception(e)
+                logger.debug('Retrying in %i seconds' % config['app']['retry_timer'])
+                self.connection = None
+                self.channel = None
+                self.connected = False
+                time.sleep(config['app']['retry_timer'])
+            except AttributeError as e:
+                logger.exception(sys.exc_info()[1])
+                logger.error("This could be caused by a problem with the RabbitMQ server!! We are going to restart the connection to be safe!!")
+                self.connection = None
+                self.channel = None
+                self.connected = False
+            except MessageNotSent as e:
+                logger.exception(e)
+                logger.error("MessageNotSent exception - this could be caused by RabbitMQ shutting down, or the queue being deleted. Resetting connection to be sure.")
+                self.connection = None
+                self.channel = None
+                self.connected = False
+            except UserWarning as e:
+                logger.exception(e)
+                logger.error('UserWarning exception - this could be caused by a connection error with RabbitMQ. Resetting the connection to be sure.')
+                self.connection = None
+                self.channel = None
+                self.connected = False
+
     def run(self):
         logger = self.get_logger()
         logger.info("Starting")
@@ -45,90 +119,14 @@ class Main(object):
 
         hostname = stats.hostname()
 
-        
+        self.config['queue']['name'] = self.config['queue']['prefix'] + '.' + hostname
 
-        config = self.config
-
-        config['queue']['name'] = config['queue']['prefix'] + '.' + hostname
-
-        connected = False
-        connection = None
-        channel = None
-
-        # used as a counter to determine interval to
-        # send uptime and ip address updates
-        meta_refresh_counter = config['app']['meta_timer']
-
-        sleep = config['app']['timer']
+        self.connected = False
+        self.connection = None
+        self.channel = None
 
         try:
-
-            while True:
-
-                try:
-
-                    if connected == False:
-                        logger.info("Creating connection")
-                        logger.debug("get connection")
-                        connection = get_connection(self.config['server'])
-
-                        logger.debug("get channel")
-                        channel = connection.channel()
-
-                        logger.debug("setup queue")
-                        setup_queue(channel, self.config['exchange'], self.config['queue'])
-
-                        connected = True
-
-                    packet = {
-                        'hostname': hostname,
-                        'cpu': stats.cpu(),
-                        'mem': stats.mem()
-                    }
-
-                    if meta_refresh_counter == config['app']['meta_timer']:
-                        packet['ipaddr'] = stats.ip4_addresses()
-                        packet['uptime'] = stats.uptime()
-                        meta_refresh_counter = 1
-
-                    if config['cpu_temp']['enabled']:
-                        packet['cpu_temp'] = stats.cpu_temp()
-
-                    packet_json = json.dumps(packet)
-                    logger.debug('Sending packet: %s' % packet_json)
-                    send_json(packet_json, channel, config['exchange']['name'], config['queue']['name'])
-
-                    meta_refresh_counter += 1
-
-                    time.sleep(sleep)
-
-                except (AMQPConnectionError, ChannelClosed, socket.error) as e:
-                    logger.critical("Connection error with RabbitMQ!")
-                    logger.exception(e)
-                    logger.debug('Retrying in %i seconds' % config['app']['retry_timer'])
-                    connection = None
-                    channel = None
-                    connected = False
-                    time.sleep(config['app']['retry_timer'])
-                except AttributeError as e:
-                    logger.exception(sys.exc_info()[1])
-                    logger.error("This could be caused by a problem with the RabbitMQ server!! We are going to restart the connection to be safe!!")
-                    connection = None
-                    channel = None
-                    connected = False
-                except MessageNotSent as e:
-                    logger.exception(e)
-                    logger.error("MessageNotSent exception - this could be caused by RabbitMQ shutting down, or the queue being deleted. Resetting connection to be sure.")
-                    connection = None
-                    channel = None
-                    connected = False
-                except UserWarning as e:
-                    logger.exception(e)
-                    logger.error('UserWarning exception - this could be caused by a connection error with RabbitMQ. Resetting the connection to be sure.')
-                    connection = None
-                    channel = None
-                    connected = False
-
+            self._loop()
         except KeyboardInterrupt:
             logger.warn("Received keyboard interrupt")
         except:
@@ -136,5 +134,5 @@ class Main(object):
             logger.exception(sys.exc_info()[1])
         finally:
             logger.warn("Shutting down")
-            if (connected == True):
-                connection.close()
+            if (self.connected == True):
+                self.connection.close()
