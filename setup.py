486b9f7374148945ab5df65c22af13a4785b735f
alex-dow <adow@psikon.com>
2014-10-07 00:22:22 -0400

    Changing version to 0.0.12-dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="psistats-client",
-    version="0.0.11",
+    version="0.0.12-dev",
     description="Psistats python client",
     url="http://psistats.psikon.org",
     author="Alex D",
