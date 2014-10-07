93b6f5eb24e9bce314ff12b53ddb1e21d674a1e5
v0idnull <v0idnull@gmail.com>
2014-10-07 00:18:47 -0400

    Organize code in primary and secondary tasks

--- psistats/app/main.py
+++ psistats/app/main.py
@@ -27,7 +27,7 @@ class Main(object):
         self.pidfile_path = config['app']['pidfile']
         self.pidfile_timeout = config['app']['pidfile_timeout']
 
-    def get_logger(self):
+    def _init_logger(self):
         if self.logger == None:
 
             logger_config = self.config['logging']
@@ -36,83 +36,94 @@ class Main(object):
             logger = logging.getLogger("psistats")
             self.logger = logger
         return self.logger
+    
+    def _init_connection(self):
+        self.logger.info("Creating connection")
+        self.logger.debug("get connection")
+        self.connection = get_connection(self.config['server'])
+
+        self.logger.debug("get channel")
+        self.channel = self.connection.channel()
+
+        self.logger.debug("setup queue")
+        setup_queue(self.channel, self.config['exchange'], self.config['queue'])
+
+        self.connected = True
+
+
+    def _primary_task(self):
+        packet = {
+            'hostname': stats.hostname(),
+            'cpu': stats.cpu(),
+            'mem': stats.mem()
+        }
+        if self.config['cpu_temp']['enabled']:
+            packet['cpu_temp'] = stats.cpu_temp()        
+
+        packet_json = json.dumps(packet)
+        self.logger.debug('Sending packet: %s' % packet_json)
+        send_json(packet_json, self.channel, self.config['exchange']['name'], self.config['queue']['name'])
+
+    def _secondary_task(self):
+        packet = {
+            'hostname': stats.hostname(),
+            'ipaddr': stats.ip4_addresses(),
+            'uptime': stats.uptime()
+        }
+
+        packet_json = json.dumps(packet)
+        self.logger.debug('Sending packet: %s' % packet_json)
+        send_json(packet_json, self.channel, self.config['exchange']['name'], self.config['queue']['name'])
+
 
     def _loop(self):
-        config = self.config
-        logger = self.get_logger()
         # used as a counter to determine interval to
         # send uptime and ip address updates
-        secondary_timer = config['app']['secondary_timer']
-        primary_timer = config['app']['primary_timer']
+        secondary_timer = self.config['app']['secondary_timer']
+        primary_timer = self.config['app']['primary_timer']
 
         while True:
-
             try:
-
                 if self.connected == False:
-                    logger.info("Creating connection")
-                    logger.debug("get connection")
-                    self.connection = get_connection(self.config['server'])
-
-                    logger.debug("get channel")
-                    self.channel = self.connection.channel()
-
-                    logger.debug("setup queue")
-                    setup_queue(self.channel, self.config['exchange'], self.config['queue'])
-
-                    self.connected = True
-
-                packet = {
-                    'hostname': stats.hostname(),
-                    'cpu': stats.cpu(),
-                    'mem': stats.mem()
-                }
-
-                if secondary_timer == config['app']['secondary_timer']:
-                    packet['ipaddr'] = stats.ip4_addresses()
-                    packet['uptime'] = stats.uptime()
+                    self._init_connection()
+                self._primary_task()
+                if (secondary_timer == self.config['app']['secondary_timer']):
+                    self._secondary_task()
                     secondary_timer = 1
-
-                if config['cpu_temp']['enabled']:
-                    packet['cpu_temp'] = stats.cpu_temp()
-
-                packet_json = json.dumps(packet)
-                logger.debug('Sending packet: %s' % packet_json)
-                send_json(packet_json, self.channel, config['exchange']['name'], config['queue']['name'])
-
-                secondary_timer += 1
-
+                else:
+                    secondary_timer = secondary_timer + 1
                 time.sleep(primary_timer)
-
             except (AMQPConnectionError, ChannelClosed, socket.error) as e:
-                logger.critical("Connection error with RabbitMQ!")
-                logger.exception(e)
-                logger.debug('Retrying in %i seconds' % config['app']['retry_timer'])
+                self.logger.critical("Connection error with RabbitMQ!")
+                self.logger.exception(e)
+                self.logger.debug('Retrying in %i seconds' % self.config['app']['retry_timer'])
                 self.connection = None
                 self.channel = None
                 self.connected = False
-                time.sleep(config['app']['retry_timer'])
+                time.sleep(self.config['app']['retry_timer'])
             except AttributeError as e:
-                logger.exception(sys.exc_info()[1])
-                logger.error("This could be caused by a problem with the RabbitMQ server!! We are going to restart the connection to be safe!!")
+                self.logger.exception(sys.exc_info()[1])
+                self.logger.error("This could be caused by a problem with the RabbitMQ server!! We are going to restart the connection to be safe!!")
                 self.connection = None
                 self.channel = None
                 self.connected = False
             except MessageNotSent as e:
-                logger.exception(e)
-                logger.error("MessageNotSent exception - this could be caused by RabbitMQ shutting down, or the queue being deleted. Resetting connection to be sure.")
+                self.logger.exception(e)
+                self.logger.error("MessageNotSent exception - this could be caused by RabbitMQ shutting down, or the queue being deleted. Resetting connection to be sure.")
                 self.connection = None
                 self.channel = None
                 self.connected = False
             except UserWarning as e:
-                logger.exception(e)
-                logger.error('UserWarning exception - this could be caused by a connection error with RabbitMQ. Resetting the connection to be sure.')
+                self.logger.exception(e)
+                self.logger.error('UserWarning exception - this could be caused by a connection error with RabbitMQ. Resetting the connection to be sure.')
                 self.connection = None
                 self.channel = None
                 self.connected = False
 
+
+
     def run(self):
-        logger = self.get_logger()
+        logger = self._init_logger()
         logger.info("Starting")
 
         warnings.filterwarnings('error', '.*Write buffer exceeded warning threshold.*')
