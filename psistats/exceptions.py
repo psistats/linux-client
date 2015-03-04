5c94b871fe378f4a7f8d05c8d8ca81aa3d4bdfd3
v0idnull <v0idnull@gmail.com>
2015-03-03 22:11:50 -0500

    Added some new exceptions Also added some moderately better handling of chained exceptions

--- psistats/exceptions.py
+++ psistats/exceptions.py
@@ -1,5 +1,21 @@
-class FileNotFoundException(BaseException):
+import traceback
+
+class PsistatsException(BaseException):
+    def __init__(self, message, cause=None):
+        if (cause != None):
+            message = message + ', caused by:' + "\n" + traceback.format_exc()
+
+        super(PsistatsException, self).__init__(message)
+        self.cause = cause
+
+class QueueConfigException(PsistatsException):
     pass
 
-class MessageNotSent(BaseException):
+class QueueConnectionException(PsistatsException):
+    pass
+
+class FileNotFoundException(PsistatsException):
+    pass
+
+class MessageNotSentException(PsistatsException):
     pass
