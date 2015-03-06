a870ff5dd39f5b221bdd7a6d6974b16cf305faca
v0idnull <v0idnull@gmail.com>
2015-03-05 19:33:25 -0500

    Added unit tests for PsistatsException

---
+++ tests/exceptions_test.py
@@ -0,0 +1,15 @@
+import unittest
+import mock
+import traceback
+from psistats import exceptions
+
+class ExceptionTest(unittest.TestCase):
+    
+    def _stacktrace():
+        return "foobar"
+
+    @mock.patch('traceback.format_exc', side_effect=_stacktrace)
+    def test_causeAppended(self, mock_format_exc):
+        e = exceptions.PsistatsException("first", "second")
+        self.assertEquals(e.cause, "second")
+        self.assertEquals(e.message, "first, caused by:\nfoobar")
