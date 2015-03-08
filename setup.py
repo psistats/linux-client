9d89d1bcf77ddacd1c34c71375a64a879505b271
v0idnull <v0idnull@gmail.com>
2015-03-08 16:45:02 -0400

    Revert version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="psistats-client",
-    version="0.2.1-dev",
+    version="0.0.12-dev",
     description="Psistats python client",
     url="http://psistats.psikon.org",
     author="Alex D",
