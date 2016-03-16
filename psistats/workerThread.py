7bf8267a69819bffbde98ca41c5e6e3fe855f4b7
alex-dow <adow@psikonc.om>
2016-03-16 00:38:55 -0400

    Each reporting thread needs its own rabbitmq connection

---
+++ psistats/workerThread.py
@@ -0,0 +1,48 @@
+import time
+from threading import Thread
+import logging
+import json
+from queue import Queue
+
+
+class WorkerThread(Thread):
+    def __init__(self, interval, worker, config):
+        self._worker = worker
+        self._config = config
+        self._running = False
+        self._interval = interval
+        self._counter = interval
+        self._logger = logging.getLogger('psistats')
+        super(WorkerThread, self).__init__()
+
+    def start(self):
+        self._queue = Queue(self._config['server']['url'], self._config['queue'], self._config['exchange'])
+        self._queue.start()
+        super(WorkerThread, self).start()
+
+    def stop(self):
+        self._running = False
+        self.join()
+        self._queue.stop()
+       
+    def send_packet(self, obj):
+        packet = json.dumps(obj)
+        self._logger.debug(packet)
+        self._queue.send(json.dumps(obj))
+
+    def loop(self):
+        if self._counter == self._interval:
+            self.send_packet(self._worker(self._config))
+            self._counter = 0
+        else:
+            time.sleep(1)
+            self._counter += 1       
+       
+
+    def run(self):
+        self._running = True
+
+        while self._running == True:
+            self.loop()
+
+
